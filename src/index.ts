2113832d177e841e9bd83cf89f6b7d92745a8335
Jan Matzek <75334114+janmatzek@users.noreply.github.com>
2024-11-24 18:32:08 +0100

    rename main endpoint, improve error handling, apply prettier (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,94 @@
-import express, { Request, Response, NextFunction } from "express";
-import cors from "cors";
-import { findDates, findPlaces } from "./inputProcessing";
-import { isDailyWeatherData } from "./utils";
-import { getLogger } from "./logging";
+import express, { Request, Response, NextFunction } from 'express'
+import cors from 'cors'
+import { findDates, findPlaces } from './inputProcessing'
+import { isDailyWeatherData } from './utils'
+import { getLogger } from './logging'
 import {
-  geocodeLocation,
-  getTimezone,
-  getWeatherData,
-  generateDailyWeatherForecastMessage,
-} from "./weatherAndGeo";
+    geocodeLocation,
+    getTimezone,
+    getWeatherData,
+    generateDailyWeatherForecastMessage,
+} from './weatherAndGeo'
 
-const logger = getLogger();
-logger.info("Starting server...");
-const app = express();
-app.use(cors());
-const port = process.env.PORT || 3000;
+const logger = getLogger()
+logger.info('Starting server...')
+const app = express()
+app.use(cors())
+const port = process.env.PORT || 3000
 
-app.use(express.json());
-app.get("/", async (req: Request, res: Response) => {
-  logger.info("Hello, world!");
-  res.json({ message: "Hello, world!" });
-});
-app.post("/test", async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    logger.info("Received request on endpoint /test");
-    const body = req.body;
-    const userInput = body.userInput;
-    if (!userInput) {
-      throw new Error("Sorry, but I can't work with that 🤷");
+app.use(express.json())
+app.get('/', async (req: Request, res: Response) => {
+    logger.info('Hello, world!')
+    res.json({ message: 'Hello, world!' })
+})
+app.post(
+    '/forecast',
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            logger.info('Received request on endpoint /test')
+            const body = req.body
+            const userInput = body.userInput
+            if (!userInput) {
+                throw new Error("Sorry, but I can't work with that 🤷")
+            }
+
+            const places = findPlaces(userInput)
+            if (places.length === 0) {
+                throw new Error(
+                    'I am sorry, but I could not find any place in that 🔎🗺️'
+                )
+            }
+            // TODO: handle multiple places
+            const place = places[0]
+
+            const dates = findDates(userInput)
+            if (dates.length === 0) {
+                throw new Error(
+                    'I am sorry, but I could not find any date in there 🧐📅.'
+                )
+            }
+            // TODO: handle multiple dates
+            const date = dates[0]
+
+            const geocodedLocation = await geocodeLocation(place)
+
+            const timezone = await getTimezone(
+                geocodedLocation.latitude,
+                geocodedLocation.longitude
+            )
+
+            const weatherData = await getWeatherData(
+                geocodedLocation.latitude,
+                geocodedLocation.longitude,
+                timezone,
+                date,
+                'daily'
+            )
+
+            if (isDailyWeatherData(weatherData)) {
+                const weatherMessage = generateDailyWeatherForecastMessage(
+                    weatherData,
+                    geocodedLocation,
+                    date
+                )
+
+                res.json({ message: weatherMessage })
+            } else {
+                throw new Error('Hourly weather data not supported yet')
+            }
+        } catch (error) {
+            logger.error('Error in /test endpoint', error)
+            next(error)
+        }
     }
-
-    const places = findPlaces(userInput);
-    if (places.length === 0) {
-      throw new Error(
-        "I am sorry, but I could not find any place in that 🔎🗺️"
-      );
-    }
-    // TODO: handle multiple places
-    const place = places[0];
-    console.log(place);
-
-    const dates = findDates(userInput);
-    if (dates.length === 0) {
-      throw new Error(
-        "I am sorry, but I could not find any date in there 🧐📅."
-      );
-    }
-    // TODO: handle multiple dates
-    const date = dates[0];
-    console.log(date);
-
-    const geocodedLocation = await geocodeLocation(place);
-    console.log(geocodedLocation);
-
-    const timezone = await getTimezone(
-      geocodedLocation.latitude,
-      geocodedLocation.longitude
-    );
-    console.log(timezone);
-
-    const weatherData = await getWeatherData(
-      geocodedLocation.latitude,
-      geocodedLocation.longitude,
-      timezone,
-      date,
-      "daily"
-    );
-
-    console.log(weatherData);
-    if (isDailyWeatherData(weatherData)) {
-      const weatherMessage = generateDailyWeatherForecastMessage(
-        weatherData,
-        geocodedLocation,
-        date
-      );
-
-      console.log(weatherMessage);
-
-      console.log("Sending response on endpoint /test");
-      res.json({ message: weatherMessage });
-    } else {
-      throw new Error("Hourly weather data not supported yet");
-    }
-  } catch (error) {
-    logger.error("Error in /test endpoint", error);
-    next(error);
-  }
-});
+)
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).json({ error: err.message });
-});
+    console.error(err.stack)
+    res.status(500).json({ error: err.message })
+})
 
 app.listen(port, () => {
-  console.log(`🦊💻 Server running at http://localhost:${port}`);
-});
+    logger.info(`🦊💻 Server running at http://localhost:${port}`)
+})
--- src/types.ts
+++ src/types.ts
@@ -1,35 +1,35 @@
 export type GetDataParams = {
-  url: string;
-  headers?: Record<string, string>;
-  params?: Record<string, any>;
-};
+    url: string
+    headers?: Record<string, string>
+    params?: Record<string, any>
+}
 
 export type Place = {
-  name: string;
-  displayName: string;
-  latitude: number;
-  longitude: number;
-  error?: string;
-};
+    name: string
+    displayName: string
+    latitude: number
+    longitude: number
+    error?: string
+}
 
 export type DailyWeatherData = {
-  time: string;
-  temperature2mMax: number;
-  temperature2mMin: number;
-  sunrise: string;
-  sunset: string;
-  sunshineDuration: number;
-  precipitationProbabilityMax: number;
-  windSpeed10mMax: number;
-};
+    time: string
+    temperature2mMax: number
+    temperature2mMin: number
+    sunrise: string
+    sunset: string
+    sunshineDuration: number
+    precipitationProbabilityMax: number
+    windSpeed10mMax: number
+}
 
 export type HourlyWeatherData = {
-  time: Array<string>;
-  temperature2m: Array<number>;
-  relativeHumidity2m: Array<number>;
-  apparentTemperature: Array<number>;
-  precipitationProbability: Array<number>;
-  precipitation: Array<number>;
-  cloudCover: Array<number>;
-  visibility: Array<number>;
-};
+    time: Array<string>
+    temperature2m: Array<number>
+    relativeHumidity2m: Array<number>
+    apparentTemperature: Array<number>
+    precipitationProbability: Array<number>
+    precipitation: Array<number>
+    cloudCover: Array<number>
+    visibility: Array<number>
+}
