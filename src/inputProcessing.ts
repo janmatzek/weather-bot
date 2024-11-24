a61912808fa7b9d2bb0b97981476a3faa0c3e5d4
janmatzek <75334114+janmatzek@users.noreply.github.com>
2024-11-24 18:31:38 +0100

    rename main endpoint, improve error handling, apply prettier

--- src/inputProcessing.ts
+++ src/inputProcessing.ts
@@ -1,24 +1,25 @@
-import nlp from "compromise";
-import datesPlugin from "compromise-dates";
+import nlp from 'compromise'
+import datesPlugin from 'compromise-dates'
+import { cities } from './lexicon'
 
-nlp.extend(datesPlugin);
+nlp.extend(datesPlugin)
 
 export function findPlaces(input: string): Array<string> {
-  const doc = nlp(input);
-  const places = doc.places();
-  return places.out("array");
+    const doc = nlp(input, cities)
+    const places = doc.places()
+    return places.out('array')
 }
 
 //TODO: figure out why the returned date is date - 1 for dates like November 4th
 export function findDates(input: string): Array<string> {
-  const doc: any = nlp(input);
-  const dates = doc.dates().json();
-  const fromattedDates: Array<string> = [];
+    const doc: any = nlp(input)
+    const dates = doc.dates().json()
+    const fromattedDates: Array<string> = []
 
-  for (let i = 0; i < dates.length; i++) {
-    const date = dates[i].dates.start.split("T")[0];
-    fromattedDates.push(date);
-  }
+    for (let i = 0; i < dates.length; i++) {
+        const date = dates[i].dates.start.split('T')[0]
+        fromattedDates.push(date)
+    }
 
-  return fromattedDates;
+    return fromattedDates
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,36 +1,36 @@
-import axios from "axios";
-import { GetDataParams } from "./types";
-import { DailyWeatherData, HourlyWeatherData } from "./types";
+import axios from 'axios'
+import { GetDataParams } from './types'
+import { DailyWeatherData, HourlyWeatherData } from './types'
 /**
  *
  * @param GetDataParams - object containing URL, headers and params
  * @returns Returns data object from the response.
  */
 export async function requestDataViaAxiosGet({
-  url,
-  headers,
-  params,
+    url,
+    headers,
+    params,
 }: GetDataParams): Promise<Record<string, any>> {
-  try {
-    const response = await axios.get(url, {
-      params,
-      headers,
-      timeout: 3 * 1000,
-    });
-    return response.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      throw new Error(
-        `Error fetching data: ${error.message}, ${error.response?.data}`
-      );
-    } else {
-      throw new Error(`An error occurred while fetching data: ${error}`);
+    try {
+        const response = await axios.get(url, {
+            params,
+            headers,
+            timeout: 3 * 1000,
+        })
+        return response.data
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            throw new Error(
+                `Error fetching data: ${error.message}, ${error.response?.data}`
+            )
+        } else {
+            throw new Error(`An error occurred while fetching data: ${error}`)
+        }
     }
-  }
 }
 
 export function isDailyWeatherData(
-  data: DailyWeatherData | HourlyWeatherData
+    data: DailyWeatherData | HourlyWeatherData
 ): data is DailyWeatherData {
-  return (data as DailyWeatherData).temperature2mMax !== undefined;
+    return (data as DailyWeatherData).temperature2mMax !== undefined
 }
